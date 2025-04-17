f7533a97dc121c7c71df2d4c309659873eb9352f
Antonio Alves <antonio.alves@duque.g12.br>
2025-04-17 19:06:30 -0300

    Criado o controller da aplicação

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -21,5 +21,7 @@ function handleRequest(){
 // Função que exibe as tasks na view
 function showTasks(){
     // Seu código aqui
+    $tasks = getTasks();
+    require_once __DIR__.'/../views/tasks.php';
 }
 ?>
